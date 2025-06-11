a3ea2b4a8e02cc7c1fafc51e950e77883213ca8d
marvi <markus@harnvi.net>
2025-06-12 00:00:41 +0200

    Fixing JTE issues

--- web/src/main/java/lectio/controller/IndexController.java
+++ web/src/main/java/lectio/controller/IndexController.java
@@ -1,16 +1,22 @@
 package lectio.controller;
 
+import gg.jte.ContentType;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.logging.Logger;
+
 @Controller
 public class IndexController {
 
+  Logger log = Logger.getLogger(IndexController.class.getName());
+
   @GetMapping("/")
   public String view(Model model, HttpServletResponse response) {
-    model.addAttribute("model", "demo");
+    log.info("Index mapping called " + ContentType.Html);
+    model.addAttribute("hello", "demo");
     return "index";
   }
 }
